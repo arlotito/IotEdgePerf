2828cddd455e51c431980eabb16f5653c16ff321
arlotito <arlotito@microsoft.com>
2021-10-04 07:53:45 +0200

    run id and extended info for bursts/run

--- edgeSolution/modules/source/message.cs
+++ edgeSolution/modules/source/message.cs
@@ -6,11 +6,19 @@ namespace edgeBenchmark
     {
         // timestamp
         public long ts;
+
+        public int runMsgTotal;
+        public int runMsgCounter;
+        public double runElapsed;
+
+        public int burstCounter;
         // msg counter
-        public int counter;
+        public int burstMsgCounter;
         
         // total msg to be sent
-        public int total;
+        public int burstLength;
+
+        public double burstElapsed;
 
         // a guid for this run
         public string runId;
