ce2e18e85271f7abee0fe912adf7f32633134147
arlotito <arlotito@microsoft.com>
2021-10-27 19:18:12 +0200

    moved to IotEdgePerf.Shared namespace

---
+++ source/IotEdgePerf.Shared/TransmitterConfigData.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+
+namespace IotEdgePerf.Shared
+{
+    public class TransmitterConfigData
+    {
+        public bool autoStart;
+        public int burstLength;
+        public int burstWait;
+        public int burstNumber;
+        public int targetRate;
+        public int payloadLength;
+
+        public int batchSize;
+        public bool logMsg;
+        public bool logBurst;
+        public bool logHist;
+
+        public int waitBeforeStart;
+
+        public int rateCalcPeriod;
+
+        public static TransmitterConfigData GetFromJson(string configDataJson)
+        {
+            return JsonConvert.DeserializeObject<TransmitterConfigData>(configDataJson);
+        }
+
+        public static TransmitterConfigData GetFromObject(object obj)
+        {
+            return JsonConvert.DeserializeObject<TransmitterConfigData>(JsonConvert.SerializeObject(obj));
+        }
+    }
+}
